87fbb3ae96ddfa36b85868193dfd589213fd353b
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-07-21 16:08:27 +0200

    ## LS 过滤器 (第 5 个习题，共 13 个)
      编写一个程序来打印出指定目录下的文件列表，并且以特定的文件名扩展名来过滤这
      个列表。这次会有两个参数提供给你，第一个是所给的文件目录路径（如：path/to/
      dir），第二个参数则是需要过滤出来的文件的扩展名。
    
      举个例子：如果第二个参数是 txt，那么你需要过滤出那些扩展名为 .txt的文件。
    
      注意，第二个参数将不会带有开头的 .。
    
      你需要在终端中打印出这个被过滤出来的列表，每一行一个文件。另外，你必须使用
      异步的 I/O 操作。

---
+++ README.md
@@ -0,0 +1,1 @@
+this is the test of learnyounode in nodejs School.
--- filtered.js
+++ filtered.js
@@ -1,2 +1,56 @@
 var fs = require('fs');
 var path = require('path');
+
+var extention = '.' + process.argv[3];
+var dir = process.argv[2];
+// fs.readdir(dir,function(err, list){
+//   console.log(list);
+// });
+//
+fs.readdir(dir,(err,list)=>{
+  if (err) throw err;
+  // else if (list.path.extname) {
+  //
+  // }
+  // console.log(list);
+  for (var i in list) {
+    if (path.extname(list[i]) == extention ) {
+      console.log(list[i]);
+    }
+  }
+
+//   fs.readdir(folder, function (err, files) {
+//   if (err) return console.error(err)
+//   files.forEach(function(file) {
+//       if (path.extname(file) === ext) {
+//           console.log(file)
+//       }
+//   })
+// })
+
+  //
+  // for (var i = 0; i < list.length; i++) {
+  //   if (path.extname(list[i]) == '.js') console.log(list[i]);
+  // }
+})
+
+// 要过滤掉对象继承的属性，用hasOwnProperty()来实现：
+//
+// var o = {
+//     name: 'Jack',
+//     age: 20,
+//     city: 'Beijing'
+// };
+// for (var key in o) {
+//     if (o.hasOwnProperty(key)) {
+//         alert(key); // 'name', 'age', 'city'
+//     }
+// }
+// 由于Array也是对象，而它的每个元素的索引被视为对象的属性，因此，for ... in循环可以直接循环出Array的索引：
+//
+// var a = ['A', 'B', 'C'];
+// for (var i in a) {
+//     alert(i); // '0', '1', '2'
+//     alert(a[i]); // 'A', 'B', 'C'
+// }
+// 请注意，for ... in对Array的循环得到的是String而不是Number。
